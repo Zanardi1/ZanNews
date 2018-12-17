92430c4d9d57f1acc7895a0af7182ac0c14ecf69
Bogdan Doicin <alex.zanardi@gmail.com>
2018-12-17 12:40:31 +0200

    Add to RSSTools

--- ZanScore/RSSTools.cs
+++ ZanScore/RSSTools.cs
@@ -18,6 +18,33 @@ O biblioteca ce contine toate functiile necesare prelucrarii unui fisier RSS:
     internal class RSSTools
     {
         readonly string FileToProcess;
+        string[] FileContent;
+
+        public struct Item
+        //O structura ce retine o singura stire
+        //todo: Sa adaug si restul subcategoriilor, odata ce am o aplicatie functionala
+        {
+            public string Title; //Titlul stirii
+            public string Link; //Link-ul catre stire
+            public string Description; //Descrierea stirii
+        }
+
+        public struct RSSData
+        // O structura ce retine datele dintr-un fisier RSS. Detalii despre acesta sunt la https://www.w3schools.com/xml/xml_rss.asp.
+        //Deoarece aceste campuri sunt citire dintr-un fisier, toti membrii acestei structuri sunt readonly
+        //todo: Sa adaug si restul subcategoriilor, odata ce am o aplicatie functionala
+        {
+            public float RSSVersion; //Versiunea de RSS folosita
+            public string ChannelTitle; //Titlul canalului
+            public string ChannelLink; //Link catre URL-ul canalului
+            public string ChannelDescription; //Descrierea canalului
+            public string Category; //Categoria stirilor
+            public string Copyright; //Informatii despre copyright
+            public string Language; //Limba in care sunt scrise stirile
+            public string PubDate; //Data publicarii stirii
+            public string ManagingEditor; //E-mailul editorului continutului fisierului RSS
+            public Item[] AllNews; //Retine toate stirile
+        }
 
         public RSSTools(string File)
         //Constructor. Parametrul reprezinta fisierul care va fi citit
@@ -40,10 +67,20 @@ O biblioteca ce contine toate functiile necesare prelucrarii unui fisier RSS:
         public string[] ReadRSSContent()
         //Citeste continutul fisierului RSS
         {
-            string[] text = File.ReadAllLines(FileToProcess);
-            return text;
+            FileContent = File.ReadAllLines(FileToProcess);
+            return FileContent;
         }
 
+        public bool ValidateRSSFile()
+        //Valideaza fisierul RSS. 
+        //todo: De gandit si de scris detaliile subrutinei
+        {
+            return true;
+        }
 
+        public void FillRSSData()
+        {
+            RSSData Data;
+        }
     }
 }
