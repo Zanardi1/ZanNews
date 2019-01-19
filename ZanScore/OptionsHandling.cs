1968007f09330ecc4e9fa169d083295f9b40d1ba
Bogdan Doicin <alex.zanardi@gmail.com>
2019-01-19 16:34:14 +0200

    Began working on Options window

---
+++ ZanScore/OptionsHandling.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Clasa ce se ocpa de manipularea fisierului de optiuni si de punerea in aplicare a optiunilor citite.
+ Proprietati:
+ 
+     1. WindowsStartup. true daca aplicatia porneste odata cu Windows, false altfel
+     2. MinimizeToTray. true daca aplicatia se minimizeaza in SysTray, false altfel
+     3. StartupOptions. 1 daca aplicatia va porni cu fereastra minimizata, 2 daca va porni cu fereastra la dimensiunile implicite, 3 daca va porni cu fereastra maximizata
+     4. DisableInvalidNewsFiles. true daca dezactiveaza sursele de stiri care au fisiere RSS invalide, false altfel
+     5. WindowWidth. Retine latimea ferestrei atunci cand programul a fost inchis
+     6. WindowHeight. Acelasi lucru, dar pentru inaltimea ferestrei
+     7. WindowLeft. Acelasi lucru pentru distanta de la marginea din stanga a ecranului la partea din stanga a ferestrei
+     8. WindowTop. Acelasi lucru pentru distanta de la marginea de sus a ecranului la partea de sus a ferestrei
+     
+Metode:
+     
+     1.Deschiderea fisierului de optiuni
+     2.Verificarea existentei lui si, daca el nu exista, crearea unuia nou, cu optiuni implicite
+     3.Salvarea optiunilor in fisier
+     4.Citirea optiunilor din fisier (atribuirea valorilor citite din fisier catre proprietatile clasei 
+     5.Afisarea acestora in ferastra de optiuni
+     6.Verificarea integritatii continutului fisierului de optiuni*/
+
+namespace ZanScore
+{
+    class OptionsHandling
+    {
+        bool WindowsStartup;
+        bool MinimizeToTray;
+        int StartupOptions;
+        bool DisableInvalidNewsFiles;
+        int WindowWidth;
+        int WindowHeight;
+        int WindowLeft;
+        int WindowTop;
+
+        public OptionsHandling()
+        {
+
+        }
+
+        public void OpenOptionsFile()
+        {
+
+        }
+
+        public bool CheckOptionsFileExistence()
+        {
+            return true;
+        }
+
+        public void SaveOptionsToFile()
+        {
+
+        }
+
+        public void ReadOptionsFromFile()
+        {
+
+        }
+
+        public void ShowOptionsInOptionsWindow()
+        {
+
+        }
+
+        public bool CheckOptionsFileContent()
+        {
+            return true;
+        }
+    }
+}
--- ZanScore/OptionsWindow.Designer.cs
+++ ZanScore/OptionsWindow.Designer.cs
@@ -28,18 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.StartWithWindowsCheckBox = new System.Windows.Forms.CheckBox();
+            this.MinimizeToTrayCheckBox = new System.Windows.Forms.CheckBox();
+            this.AppStartGB = new System.Windows.Forms.GroupBox();
+            this.StartMinimized = new System.Windows.Forms.RadioButton();
+            this.StartNormal = new System.Windows.Forms.RadioButton();
+            this.StartMaximized = new System.Windows.Forms.RadioButton();
+            this.DisableBadSources = new System.Windows.Forms.CheckBox();
+            this.AppStartGB.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // StartWithWindowsCheckBox
+            // 
+            this.StartWithWindowsCheckBox.AutoSize = true;
+            this.StartWithWindowsCheckBox.Location = new System.Drawing.Point(34, 43);
+            this.StartWithWindowsCheckBox.Name = "StartWithWindowsCheckBox";
+            this.StartWithWindowsCheckBox.Size = new System.Drawing.Size(148, 21);
+            this.StartWithWindowsCheckBox.TabIndex = 0;
+            this.StartWithWindowsCheckBox.Text = "Start with Windows";
+            this.StartWithWindowsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MinimizeToTrayCheckBox
+            // 
+            this.MinimizeToTrayCheckBox.AutoSize = true;
+            this.MinimizeToTrayCheckBox.Location = new System.Drawing.Point(34, 83);
+            this.MinimizeToTrayCheckBox.Name = "MinimizeToTrayCheckBox";
+            this.MinimizeToTrayCheckBox.Size = new System.Drawing.Size(128, 21);
+            this.MinimizeToTrayCheckBox.TabIndex = 1;
+            this.MinimizeToTrayCheckBox.Text = "Minimize to tray";
+            this.MinimizeToTrayCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // AppStartGB
+            // 
+            this.AppStartGB.Controls.Add(this.StartMaximized);
+            this.AppStartGB.Controls.Add(this.StartMinimized);
+            this.AppStartGB.Controls.Add(this.StartNormal);
+            this.AppStartGB.Location = new System.Drawing.Point(34, 128);
+            this.AppStartGB.Name = "AppStartGB";
+            this.AppStartGB.Size = new System.Drawing.Size(207, 150);
+            this.AppStartGB.TabIndex = 2;
+            this.AppStartGB.TabStop = false;
+            this.AppStartGB.Text = "Application start options";
+            // 
+            // StartMinimized
+            // 
+            this.StartMinimized.AutoSize = true;
+            this.StartMinimized.Location = new System.Drawing.Point(18, 30);
+            this.StartMinimized.Name = "StartMinimized";
+            this.StartMinimized.Size = new System.Drawing.Size(125, 21);
+            this.StartMinimized.TabIndex = 3;
+            this.StartMinimized.TabStop = true;
+            this.StartMinimized.Text = "Start minimized";
+            this.StartMinimized.UseVisualStyleBackColor = true;
+            // 
+            // StartNormal
+            // 
+            this.StartNormal.AutoSize = true;
+            this.StartNormal.Location = new System.Drawing.Point(18, 71);
+            this.StartNormal.Name = "StartNormal";
+            this.StartNormal.Size = new System.Drawing.Size(106, 21);
+            this.StartNormal.TabIndex = 3;
+            this.StartNormal.TabStop = true;
+            this.StartNormal.Text = "Start normal";
+            this.StartNormal.UseVisualStyleBackColor = true;
+            // 
+            // StartMaximized
+            // 
+            this.StartMaximized.AutoSize = true;
+            this.StartMaximized.Location = new System.Drawing.Point(18, 113);
+            this.StartMaximized.Name = "StartMaximized";
+            this.StartMaximized.Size = new System.Drawing.Size(128, 21);
+            this.StartMaximized.TabIndex = 4;
+            this.StartMaximized.TabStop = true;
+            this.StartMaximized.Text = "Start maximized";
+            this.StartMaximized.UseVisualStyleBackColor = true;
+            // 
+            // DisableBadSources
+            // 
+            this.DisableBadSources.AutoSize = true;
+            this.DisableBadSources.Location = new System.Drawing.Point(34, 296);
+            this.DisableBadSources.Name = "DisableBadSources";
+            this.DisableBadSources.Size = new System.Drawing.Size(300, 21);
+            this.DisableBadSources.TabIndex = 3;
+            this.DisableBadSources.Text = "Disable news sources with invalid RSS files";
+            this.DisableBadSources.UseVisualStyleBackColor = true;
             // 
             // OptionsWindow
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DisableBadSources);
+            this.Controls.Add(this.AppStartGB);
+            this.Controls.Add(this.MinimizeToTrayCheckBox);
+            this.Controls.Add(this.StartWithWindowsCheckBox);
             this.Name = "OptionsWindow";
             this.Text = "Options";
+            this.AppStartGB.ResumeLayout(false);
+            this.AppStartGB.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox StartWithWindowsCheckBox;
+        private System.Windows.Forms.CheckBox MinimizeToTrayCheckBox;
+        private System.Windows.Forms.GroupBox AppStartGB;
+        private System.Windows.Forms.RadioButton StartMinimized;
+        private System.Windows.Forms.RadioButton StartMaximized;
+        private System.Windows.Forms.RadioButton StartNormal;
+        private System.Windows.Forms.CheckBox DisableBadSources;
     }
 }
