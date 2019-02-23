5c07ae7edc862e3c728b12a4cbd37daf30715cb2
Bogdan Doicin <alex.zanardi@gmail.com>
2019-02-23 16:55:16 +0200

    Creating unit test for main window

--- ZanScoreTest/ZanScoreTest.cs
+++ ZanScoreTest/ZanScoreTest.cs
@@ -1,5 +1,43 @@
 ﻿using System;
+using ZanScore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZanScore.Tests
+{
+    [TestClass()]
+    public class ZanScoreTest
+    {
+        [TestMethod()]
+        public void SetTimerEngineTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void CheckForNetworkTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void Form1Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void DownloadAllNewsProcessTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void LoadingNewsEngineTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
 
 namespace ZanScoreTest
 {
