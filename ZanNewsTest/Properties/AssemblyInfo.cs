30c5f1dc5d7afc33a826278a5185d3a70cf83b2b
Bogdan Doicin <alex.zanardi@gmail.com>
2019-02-25 16:55:51 +0200

    Began writing code for testing units

--- ZanNewsTest/Properties/AssemblyInfo.cs
+++ ZanNewsTest/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ZanScoreTest")]
+[assembly: AssemblyTitle("ZanNewsTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZanScoreTest")]
+[assembly: AssemblyProduct("ZanNewsTest")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("1a112599-a6ec-4be1-8b17-420f132b722c")]
+[assembly: Guid("c4c4cc21-3bf7-418a-b9dd-0095683170bc")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ ZanNewsTest/ZanNewsTestUnit.cs
@@ -0,0 +1,186 @@
+﻿using ZanScore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZanScore.Tests
+{
+    [TestClass()]
+    public class ZanNewsTestUnit
+    {
+        [TestMethod()]
+        public void SetTimerEngineTest()
+        {
+            Form1 f = new Form1();
+            f.OH.NewsDownloadAtInterval = 0;
+            f.SetTimerEngine();
+            Assert.AreEqual(f.DownloadNewsTimer.Enabled, false);
+        }
+
+        [TestMethod()]
+        public void CheckForNetworkTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Form1Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DownloadAllNewsProcessTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadingNewsEngineTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SelectSourcesWindowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RSSSourcesLibraryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EnableNewsSourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisableNewsSourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadSourcesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveSourcesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddNewSourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditSourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveSourceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ShowNewsSourcesInDataGridTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetNewsURLTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearSourcesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SortSourcesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RSSSourceDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OptionsWindowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OptionsHandlingTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OpenOptionsFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveOptionsToFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ReadOptionsFromFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NewsLibraryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditSourcesWindowTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddSourceWindowTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace ZanNewsTest
+{
+    [TestClass]
+    public class ZanNewsTestUnit
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
