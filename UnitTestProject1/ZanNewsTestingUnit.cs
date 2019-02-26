2307c68a8c27ab44052de761630e067a6944817e
Bogdan Doicin <alex.zanardi@gmail.com>
2019-02-26 16:41:30 +0200

    Code tweaks

---
+++ UnitTestProject1/ZanNewsTestingUnit.cs
@@ -0,0 +1,58 @@
+﻿using ZanScore;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZanScore.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void SetAutomaticDownloadTimerEngineTest()
+        {
+            int flag = 0;
+            bool Result = true;
+            Form1 f = new Form1();
+
+            Result=f.SetAutomaticDownloadTimerEngine(flag);
+
+            Assert.AreEqual(Result,false);
+        }
+
+        [TestMethod()]
+        public void CheckForNetworkTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void Form1Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DownloadAllNewsProcessTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadingNewsEngineTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ZanNewsTestingUnit
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
